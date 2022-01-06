fb0826d6b984d03a755700cada82bba058e9a402
PritDhabaliya <prit.dhabaliya@gmail.com>
2022-01-06 11:47:12 +0530

    createheader

---
+++ clientside/src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className='card bg-secondary  text-center text-light rounded-0'>
+        <h1 className='display-4'>
+            TODO-LIST
+        </h1>
+    </div>
+    );
+  }
+  
+  export default Header;
